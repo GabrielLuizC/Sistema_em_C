9352b3500e8bdcc9ed444c30e6e71eef9f2b41fa
unknown <gabriel10.speed@hotmail.com>
2021-11-07 15:25:16 -0300

    V2

--- Prova.cpp
+++ Prova.cpp
@@ -10,15 +10,18 @@ void valorEstoque();
 void quantidadeEstoque();
 void relatorio();
 void itensCadastrados();
+void listar();
+void excluirItem();
+void vendasEstoque();
+void metodoPagamento();
 
 struct cadastrar{
+	int id;
 	char nome[50];
 	float valor;
 	float quantidade;
-	float vtotal;
+	char excluir;
 };
-
-struct cadastrar cadastro;
 
 main(){
 	menu();
@@ -28,49 +31,70 @@ main(){
 void menu(){
     int escolha;
     do{
-    	printf("\n=-=-= Sistema =-=-= \n");
-	    printf("1 - Cadastrar Item \n");
-	    printf("2 - Listar Estoque \n");
-	    printf("3 - Editar Item \n");
-	    printf("4 - Excluir Item \n");
-	    printf("5 - Relatorio \n");
-	    printf("6 - Venda \n");
-	    printf("7 - Sair \n\n");
+    	printf("=-=-=-=-=-=-=-=-= Sistema =-=-=-=-=-=-=-=-=\n");
+	    printf("	1 - Cadastrar Item \n");
+	    printf("	2 - Listar e Consultar Estoque \n");
+	    printf("	3 - Editar Item \n");
+	    printf("	4 - Excluir Item \n");
+	    printf("	5 - Relatorio Geral \n");
+	    printf("	6 - Venda \n");
+	    printf("	7 - Sair\n");
+	    printf("=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-==-=-=\n\n");
 	    printf("Escolha uma das opcoes: ");
 	    
 	    scanf("%d", &escolha);   
 	    
-	        switch(escolha){
-	    case 1:
-			cadastroItem();
-	        break;
-	    case 2:
-			listarEstoque();
-		    break;
-		case 3:
-			editarItem();
-			break;
-		case 5:
-			relatorio();
-			break;
-	    case 7:
-			printf("\nSaindo ...");
-		    break;
-	    default:
-	    	printf("ERROR");
+	   switch(escolha){
+		    case 1:
+				cadastroItem();
+		        break;
+		    case 2:
+				listarEstoque();
+			    break;
+			case 3:
+				editarItem();
+				break;
+			case 4:
+				excluirItem();
+				break;
+			case 5:
+				relatorio();
+				break;
+			case 6:
+	
+				break;
+		    case 7:
+				printf("\nSAINDO DO SISTEMA ...");
+			    break;
+		    default:
+		    	printf("\n\nERROR - REINICIE O SISTEMA\n\n");
 		}	
 	}while(escolha != 7);
 }
 
 void cadastroItem(){
+	struct cadastrar cadastro;
 	int recebe;
+	
+	FILE *arq;
+    
+    arq = fopen("Estoque.pro", "ab");
+    
+    if(arq == NULL){
+    	printf("\nArquivo Nao Encontrado");
+	}
+	
+	
 	system("cls");
+	
+	printf("=-=-=-=-= Cadastro de Itens =-=-=-=-=\n");	
+	printf("Digite o ID: ");
+    scanf("%d", &cadastro.id);
 	
 	fflush(stdin);
 	
-	printf("=-=-=-=-= Cadastro de Itens =-=-=-=-=\n");		
-    printf("Digite o Nome: ");
-    fgets(cadastro.nome, 50, stdin);
+    printf("\nDigite o Nome: ");
+    gets(cadastro.nome);
     
     printf("\nDigite a Quantidade: ");
     scanf("%f", &cadastro.quantidade);
@@ -78,30 +102,16 @@ void cadastroItem(){
     printf("\nDigite o Valor Unitario do Produto: ");
     scanf("%f", &cadastro.valor);
     
-    system("cls");
+    system("cls");	
     
-    printf("%s%d \nR$ %.2f", cadastro.nome, cadastro.quantidade, cadastro.valor);
-    
-    FILE *arq;
-    
-    arq = fopen("Estoque.txt", "a");
-    
-    if(arq == NULL){
-    	printf("\nArquivo Nao Encontrado");
-	}
-	
-	system("cls");
-	
-	fprintf(arq, "Item: %sQuantidade: %.1f \nValor: R$ %.2f\n =-=-=-=-=-=-=-=-=-=-=-=-=\n", cadastro.nome, cadastro.quantidade, cadastro.valor);
+	fwrite(&cadastro, sizeof(cadastro), 1, arq);
 	
 	fclose(arq);
 	
-	printf("Item Cadastrado Com Sucesso!!\n\n");
-	cadastro.vtotal = cadastro.valor * cadastro.quantidade;
-	valorEstoque();
-	quantidadeEstoque();
-	itensCadastrados();
-	printf("Digite 1 Para Continuar!!\n");
+	printf("=-=-=-=-=-=-= Cadastro de Itens =-=-=-=-=-=-=\n");
+	printf("\n	  ITEM CADASTRADO COM SUCESSO!!\n\n");
+	printf("=-=-=-=-=-=-= Cadastro de Itens =-=-=-=-=-=-=\n\n");
+	printf("DIGITE 1 PARA CONTINUAR!!\n");
 	scanf(" ");
 	system("cls");
 	fflush(stdin);
@@ -109,12 +119,87 @@ void cadastroItem(){
 		
 
 void listarEstoque(){
-		
-	char listar[255];
+	struct cadastrar cadastro;
+	int consulta;
+	int procuraid;
+	int aux;
 	
 	FILE *arq;
 	    
-    arq = fopen("Estoque.txt", "r");
+    arq = fopen("Estoque.pro", "rb");
+    
+    if(arq == NULL){
+    	printf("\nArquivo Nao Encontrado");
+	}
+	
+	system("cls");
+	
+	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+	printf("1 - LISTAR ESTOQUE COMPLETO \n2 - CONSULTAR ITEM POR ID CADASTRADO\n\n");
+	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+	printf("Escolha uma opcao: ");
+	scanf("%d", &consulta);
+	
+	system("cls");
+	
+	if(consulta == 1){	
+		printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+		printf("LISTAGEM DE ESTOQUE COMPLETA (OBS: 'STATUS: P', INDICA QUE O ITEM NO ESTOQUE ESTA PERMITIDO PARA VISUALIZACAO)\n\n");
+		printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+		while(fread(&cadastro, sizeof(cadastro),1 , arq) != NULL){
+			if(cadastro.excluir != '*'){
+				printf("ID: %d | Item: %s | Quantidade: %.1f | Valor: R$ %.2f | Status: %c\n", cadastro.id, cadastro.nome, cadastro.quantidade, cadastro.valor, cadastro.excluir);	
+			}	
+		} 
+	}
+	if(consulta == 2){
+		printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+		printf("CONSULTA DE ITEM A PARTIR DO ID CADASTRADO NO ESTOQUE\n\n");
+		printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+		printf("DIGITE O CODIGO ID A SER CONSULTADO: ");
+		scanf("%d", &procuraid);
+		while (fread (&cadastro, sizeof(cadastro), 1, arq)){
+			if(cadastro.excluir != '*'){
+				if ((procuraid == cadastro.id)) {
+					printf("ID: %d | Item: %s | Quantidade: %.1f | Valor: R$ %.2f\n", cadastro.id, cadastro.nome, cadastro.quantidade, cadastro.valor);
+					aux = 1;
+				}	
+			}
+		}
+		
+		if (!aux) printf("\nCODIGO DIGITADO NAO FOI ENCONTRADO!!\n");
+	}
+	printf("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+	printf("\nDIGITE 1 PARA CONTINUAR!!\n");
+	scanf(" ");
+	
+	fflush(stdin);
+	system("cls");
+	fclose(arq);
+}
+
+void listar(){
+	struct cadastrar cadastro;
+	FILE *arquivo;
+	    
+    arquivo = fopen("Estoque.pro", "rb");
+    
+    while(fread(&cadastro, sizeof(cadastro),1 , arquivo) != NULL){
+    	if(cadastro.excluir != '*'){
+			printf("ID: %d | Item: %s | Quantidade: %.1f | Valor: R$ %.2f\n", cadastro.id, cadastro.nome, cadastro.quantidade, cadastro.valor);	
+		}
+	}
+    
+    fclose(arquivo);
+}
+
+void editarItem(){
+	int procuraid;
+	int aux;
+	
+	FILE *arq;
+	    
+    arq = fopen("Estoque.pro", "r+b");
     
     if(arq == NULL){
     	printf("\nArquivo Nao Encontrado");
@@ -123,138 +208,179 @@ void listarEstoque(){
 	}
 	
 	system("cls");
+
+	listar();
+
+	struct cadastrar cadastro;
 	
-	while(fgets(listar, 255, arq) != NULL) printf("%s", listar);
+	printf("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+	printf("DIGITE O CODIGO ID A SER EDITADO: ");
+	scanf("%d", &procuraid);
 	
-	printf("Digite 1 Para Continuar!!\n");
+	while (fread (&cadastro, sizeof(cadastro), 1, arq)){
+		if(cadastro.excluir != '*'){
+			if ((procuraid == cadastro.id)) {
+				printf("\nID: %d | ITEM: %s | QUANTIDADE: %.1f | VALOR: R$ %.2f\n", cadastro.id, cadastro.nome, cadastro.quantidade, cadastro.valor);
+				aux = 1;
+				
+				fseek(arq, sizeof(struct cadastrar)*-1, SEEK_CUR);
+				printf("\nDIGITE O NOVO NOME: ");
+				fflush(stdin);
+			    gets(cadastro.nome);
+			    
+			    printf("\nDIGITE A NOVA QUANTIDADE SE TIVER SOFRIDO ALTERACAO: ");
+			    scanf("%f", &cadastro.quantidade);
+			    
+			    printf("\nDIGITE O NOVO VALOR UNITARIO DO PRODUTO SE TIVER SOFRIDO ALTERACAO: ");
+			    scanf("%f", &cadastro.valor);
+			    
+			    fwrite(&cadastro, sizeof(cadastro), 1, arq);
+			    fseek(arq, sizeof(cadastro)* 0, SEEK_END);
+			}	
+		}	
+	}
+	
+	if (!aux) printf ("\nCODIGO DIGITADO NAO FOI ENCONTRADO!!\n");
+	
+	printf("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+	
+	fclose(arq);
+	
+	printf("DIGITE 1 PARA CONTINUAR!!\n");
 	scanf(" ");
 	
 	fflush(stdin);
 	system("cls");
-	fclose(arq);
 }
 
-void editarItem(){
-		
-	char listar[255];
-	char aux[255];
-	int linha;
-	int l = 1;
+void excluirItem(){
+	struct cadastrar cadastro;
+	int procuraid = 0;
+	int aux;
 	
-	FILE *arq;
-	    
-    arq = fopen("Estoque.txt", "r");
-    
-    while(fgets(listar, 255, arq) != NULL){
-		printf("Linha %d - %s", l++, listar);
+	FILE *arqs = fopen("Estoque.pro", "r+b");
+	
+	char certeza;
+	
+	system("cls");
+	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+	printf("\n\nDIGITE O CODIGO ID DO ITEM QUE SERA EXCLUIDO: ");
+	scanf("%d", &procuraid);
+	
+	while (fread (&cadastro, sizeof(cadastro), 1, arqs)){
+		if(cadastro.excluir != '*'){
+			if (procuraid == cadastro.id){
+				printf("\nID: %d | Item: %s | Quantidade: %.1f | Valor: R$ %.2f\n", cadastro.id, cadastro.nome, cadastro.quantidade, cadastro.valor);
+				aux = 1;
+				
+				printf("\nTEM CERTEZA QUE DESEJA EXCLUIR ESTE ITEM?? (s para SIM | n para NAO) \n");
+				fflush(stdin);
+				scanf("%c", &certeza);
+				if (certeza == 's'){
+					cadastro.excluir = '*';
+					printf("\nPPRODUTO EXCLUIDO COM SUCESSO!!\n");
+					fseek(arqs,sizeof(struct cadastrar)*-1, SEEK_CUR);
+					fwrite(&cadastro, sizeof(cadastro), 1, arqs);
+					fseek(arqs, sizeof(cadastro)* 0, SEEK_END);
+					//return;
+				}else if (certeza == 'n'){
+					system("cls");
+					printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+					printf("\n\nEXCLUSAO DE ITEM CANCELADA!!\n");
+					//return;
+				}	
+			}
+		}
 	}
-    
-    printf("Qual Linha Desenha Editar?? ");
-	scanf("%d", &linha);
-    
-	fgets(aux, linha+1, arq);
-	printf("TESTE: %s", aux);
+		
+		if (!aux) printf("\nCODIGO DIGITADO NAO FOI ENCONTRADO!!\n");
+		
+		printf("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+	fclose(arqs);
+	printf("\n\nDIGITE 1 PARA CONTINUAR!!\n");
+	scanf(" ");
 	
-	fclose(arq);
+	fflush(stdin);
+	system("cls");
 }
 
 void valorEstoque(){
-	float total = cadastro.vtotal;
-	float aux;
-	char listar[255];
+	struct cadastrar cadastro;
+	int count = 1;
+	float total;
+	FILE *arq;
+	    
+    arq = fopen("Estoque.pro", "rb");
+    
+	while(fread(&cadastro, sizeof(cadastro),1 , arq) != NULL){
+		if(cadastro.excluir != '*'){
+			total = total + (cadastro.quantidade * cadastro.valor);	
+		}	
+	} 
 	
-	FILE *vtotal;
-	
-	vtotal = fopen("Valor Estoque.txt", "r");
-	
-	while(fgets(listar, 255, vtotal) != NULL);
-	
-	fclose(vtotal);
-	
-	vtotal = fopen("Valor Estoque.txt", "w");
-	
-	aux = atof(listar);	
-	aux = aux + total;
-	
-	fprintf(vtotal, "%.2f", aux);
-	
-	fclose(vtotal);
+	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+	printf("\n\nVALOR TOTAL DO ESTOQUE: %.1f\n\n", total);
+	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    fclose(arq);
 	
 }
 
 void quantidadeEstoque(){
-	float total = cadastro.quantidade;
-	float aux;
-	char listar[255];
+	struct cadastrar cadastro;
+	float quant = 0;
+	FILE *arquivo;
+	    
+    arquivo = fopen("Estoque.pro", "rb");
+    
+    while(fread(&cadastro, sizeof(cadastro),1 , arquivo) != NULL) {
+    	if(cadastro.excluir != '*'){
+			quant += cadastro.quantidade;
+		}
+	}
 	
-	FILE *qtotal;
-	
-	qtotal = fopen("Quantidade Estoque.txt", "r");
-	
-	while(fgets(listar, 255, qtotal) != NULL);
-	
-	fclose(qtotal);
-	
-	qtotal = fopen("Quantidade Estoque.txt", "w");
-	
-	aux = atof(listar);	
-	aux = aux + total;
-	
-	fprintf(qtotal, "%.2f", aux);
-	
-	fclose(qtotal);
+	printf("\n\nQUANTIDADE TOTAL DE ITENS NO ESTOQUE: %.1f\n\n", quant);
+	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+    fclose(arquivo);
 }
 
 void itensCadastrados(){
-	char item[50];
+	struct cadastrar cadastro;
+	int count = 1;
+	FILE *arquivo;
+	    
+    arquivo = fopen("Estoque.pro", "rb");
+    
+    
+    while(fread(&cadastro, sizeof(cadastro),1 , arquivo) != NULL) 
+	if(cadastro.excluir != '*'){
+			printf("ITEM %d: %s\n", count++, cadastro.nome);
+	}
 	
-	item[50] = cadastro.nome[50];
-	
-	FILE *icadastro;
-	printf("%s", item);
-	icadastro = fopen("Peças Cadastradas.txt", "a");
-	
-	fprintf(icadastro, "%s", item);
-	
-	fclose(icadastro);
+	printf("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    fclose(arquivo);
 }
 
 void relatorio(){
-	FILE *vtotal;
-	char listar[50];
-	
-	vtotal = fopen("Valor Estoque.txt", "r");
 	system("cls");
-	while(fgets(listar, 255, vtotal) != NULL) printf("\nVALOR TOTAL DO ESTOQUE: %s\n\n", listar);
-	
-	fclose(vtotal);
+	valorEstoque();
 	//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
-	FILE *qtotal;
-	char listar1[50];
+	quantidadeEstoque();
+	//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+	itensCadastrados();
 	
-	qtotal = fopen("Quantidade Estoque.txt", "r");
-	while(fgets(listar1, 255, qtotal) != NULL) printf("\nQUANTIDADE TOTAL DE ITENS NO ESTOQUE: %s\n\n", listar1);
-	
-	printf("Digite 1 Para Continuar!!\n");
+	printf("\nDigite 1 Para Continuar!!\n");
 	scanf(" ");
 	system("cls");
 	fflush(stdin);
 	
-	fclose(qtotal);
-	//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
-	FILE *ptotal;
-	char listar2[50];
-	int pt = 1;
+}
+
+void vendasEstoque(){
 	
-	ptotal = fopen("Peças Cadastradas.txt", "r");
-	while(fgets(listar2, 255, qtotal) != NULL) printf("\nITEM %d: %s\n\n", pt++, listar2);
+}
+
+void metodoPagamento(){
 	
-	printf("Digite 1 Para Continuar!!\n");
-	scanf(" ");
-	system("cls");
-	fflush(stdin);
-	
-	fclose(ptotal);
 }
 		
 
--- README.md
+++
@@ -1,1 +0,0 @@
-# Sistema_em_C
